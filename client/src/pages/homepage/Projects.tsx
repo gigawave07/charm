76a856ff68e53356f1ff53915affc451e827efaf
gigawave07 <gigawave07@gmail.com>
2022-08-09 21:52:24 +0700

    Refactor Projects styled from css to styled-component

--- client/src/pages/homepage/Projects.tsx
+++ client/src/pages/homepage/Projects.tsx
@@ -1,38 +1,44 @@
-import { Col, Menu, Row } from "antd";
+import {Col, Menu, Row} from "antd";
 import React from "react";
+import style from "styled-components"
 
+const StyleWrapper = style.div.attrs({
+    className: "grid gap-10 content-center "
+})`
+  height: 1000px;
+`
 const Projects = () => {
-	const labels = ["All", "Branding", "Web Design", "Digital Marketing"];
-	const items = labels.map((item, i) => ({ label: item, key: `item-${i}` }));
+    const labels = ["All", "Branding", "Web Design", "Digital Marketing"];
+    const items = labels.map((item, i) => ({label: item, key: `item-${i}`}));
 
-	const slogan = (
-		<span>
+    const slogan = (
+        <span>
 			<p className={"heading__first-part"}>Some of our</p>
 			<p className={"heading__second-part"}>recent projects</p>
 		</span>
-	);
-	return (
-		<div className={"grid gap-10 content-center projects"}>
-			<Row>
-				<Col span={12}>
-					<Row>
-						<Col span={5} />
-						<Col>{slogan}</Col>
-					</Row>
-				</Col>
-			</Row>
-			<Row>
-				<Col span={12}>
-					<Row>
-						<Col span={5} />
-						<Col>
-							<Menu items={items} mode="horizontal" />
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-		</div>
-	);
+    );
+    return (
+        <StyleWrapper>
+            <Row className={"asd"}>
+                <Col span={12}>
+                    <Row>
+                        <Col span={5}/>
+                        <Col>{slogan}</Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={12}>
+                    <Row>
+                        <Col span={5}/>
+                        <Col>
+                            <Menu items={items} mode="horizontal"/>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </StyleWrapper>
+    );
 };
 
 export default Projects;
