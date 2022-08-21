44616d3fd3302175e0c61a33c1ef2b18f6f8e7d3
Mai Thế Vinh <maithevinhdut.ete@gmail.com>
2022-08-21 20:14:53 +0700

    move style css to style.ts

--- client/src/pages/characters/components/management/index.tsx
+++ client/src/pages/characters/components/management/index.tsx
@@ -1,24 +1,6 @@
-import { Row, Col } from "antd";
-import styled from "styled-components";
+import { Row } from "antd";
 import React, { useState } from "react";
-
-const StyleCardWrapper = styled.div`
-	width: 300px;
-	height: 300px;
-	float: left;
-	padding: 10px 10px;
-	`;
-
-const StyleCardImg = styled.img`
-	width: 100%;
-	height: 100%;
-	&:hover{
-	 transform: translate(10px, -10px);
-	 cursor: pointer}
-`;
-const StyleColCardManagement = styled(Col)`
-	margin: 0px 0px 0px 10px
-	`;
+import { StyleCardImg, StyleCardWrapper, StyleColCardManagement } from "./style";
 
 const CharactersManagement = () => {
 
---
+++ client/src/pages/characters/components/management/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Col } from "antd";
+
+export const StyleCardWrapper = styled.div`
+	width: 300px;
+	height: 300px;
+	float: left;
+	padding: 10px 10px;
+`;
+
+export const StyleCardImg = styled.img`
+	width: 100%;
+	height: 100%;
+	&:hover{
+	transform: translate(10px, -10px);
+	cursor: pointer}
+`;
+export const StyleColCardManagement = styled(Col)`
+margin: 0px 0px 0px 10px
+	`;
